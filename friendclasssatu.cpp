a63f80a46abed6613c55bc43c705db9dc8ce50e9
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-04-28 15:36:39 +0700

    Enhance siswa class with setId and displayAll methods
    Added setId method and displayAll method to siswa class.

--- friendclasssatu.cpp
+++ friendclasssatu.cpp
@@ -18,3 +18,13 @@ class siswa
 private:
     int id;
 
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+
