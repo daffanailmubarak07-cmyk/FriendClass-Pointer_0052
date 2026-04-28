27fc07d51c859c78d58f27c49d7d669c7beb78d0
daffanailmubarak07-cmyk <daffanailmubarak07@gmail.com>
2026-04-28 15:27:37 +0700

    Refactor pointdua.cpp for consistency

--- pointdua.cpp
+++ pointdua.cpp
@@ -19,8 +19,4 @@ int main()
     refMhs.nim = 2; //member access operator
     mhs.showNim();
 
-    mahasiswa *pMhs = &mhs; //pointer dereference pmhs
-    pMhs->nim = 3; //arrow operator
-    pMhs->showNim();
-    return 0;
-}
+    
